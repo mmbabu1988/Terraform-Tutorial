48770a5d9112b93497b0c5e03a39999072896985
mmbabu1988 <85672015+mmbabu1988@users.noreply.github.com>
2023-11-27 12:02:00 +0530

    Update variables.tf

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -1,9 +1,9 @@
 variable "region" {
-  default = "us-east-1"
+  default = "us-west-1"
 }
 
 variable "ami_id" {
   default = {
-     us-east-1    = "ami-06aa3f7caf3a30282"
+     us-east-1    = "ami-0cbd40f694b804622"
   }
 }
