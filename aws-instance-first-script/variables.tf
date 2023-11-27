af399abe661d7d0014151874ba8c92a10667bb27
mmbabu1988 <85672015+mmbabu1988@users.noreply.github.com>
2023-11-27 11:42:31 +0530

    Update variables.tf

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -2,8 +2,11 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "ami_id" {
-  default = {
-     us-east-1    = "ami-0fc5d935ebf8bc3bc"
+data "aws_ami_ids" "ubuntu" {
+  owners = ["308954559815"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/ubuntu-*-*-amd64-server-*"]
   }
 }
