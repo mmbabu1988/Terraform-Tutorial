7e5110c91be75a2210912352585b06dc8065641b
mmbabu1988 <85672015+mmbabu1988@users.noreply.github.com>
2023-11-27 11:01:16 +0530

    Update variables.tf

--- aws-instance-first-script/variables.tf
+++ aws-instance-first-script/variables.tf
@@ -6,6 +6,8 @@ variable "ami_id" {
   type = "map"
 
   default = {
-    us-east-1    = "ami-0fc5d935ebf8bc3bc"
+     us-east-1    = "ami-035b3c7efe6d061d5"
+     eu-west-2    = "ami-132b3c7efe6sdfdsfd"
+     eu-central-1 = "ami-9787h5h6nsn"
   }
 }
